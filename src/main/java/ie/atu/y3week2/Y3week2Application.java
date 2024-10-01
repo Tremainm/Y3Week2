23e2ecb8be3dac832a1a329f87140c1064d290e2
Tremain <G00367973@atu.ie>
2024-10-01 10:19:52 +0100

    Push master to github

---
+++ src/main/java/ie/atu/y3week2/Y3week2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.y3week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y3week2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y3week2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/y3week2/Y3week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.y3week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Y3week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
